6f03a581c3c6551f6d59b1961935d4a121025f9b
TheNobu <samueldosreis1234@gmail.com>
2023-06-20 15:55:44 -0300

    checkbox

--- test/src/pages/Login.js
+++ test/src/pages/Login.js
@@ -2,23 +2,42 @@ import Input from '../components/Input';
 import './Login.css'
 import {useState} from 'react'
 
-const [email,setEmail]= useState()
+const Login = () => {
+
+const [email,setEmail] = useState()
 const [password,setPassword] = useState()
+const [check,setCheck] = useState(false)
 
 const onChangeEmail = (event) =>{
     setEmail(event.target.value)
 }
 
 const onChangePassword = (event) =>{
-    
+    setPassword(event.target.value)
 }
-
-const Login = () => {
+const onSubmit = () =>{
+    console.log(email,password)
+}
+const showPassword = () =>{
+    setCheck(!check)
+}
     return (
-        <div>
+        <div className='container'>
             <Input
-            
+            label = "E-mail"
+            name = "emailInput"
+            value={email}
+            onChange={onChangeEmail}
             />
+            <Input
+            label= "Password"
+            name = "passwordInput"
+            type = {check ? "text": "Password"}
+            value = {password}
+            onChange={onChangePassword}
+            />
+            <button onClick = {onSubmit}>Login</button>
+            <input type= 'checkbox' onChange = {showPassword}></input>
         </div>
     );
 }
